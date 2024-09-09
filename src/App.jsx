c2c56ec59617d667863e8cb20cd00db08c02ae1e
joy <joypal.hazard@gmail.com>
2024-09-09 10:56:01 +0600

    commit 9/9/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 const App = () => {
   const [advice, setAdvice] = useState({
     advice: "",
     isFetching: false,
   });
-
+  const [quoteCount, setQuoteCount] = useState(0); // New state to track quote count
 
   // Fetch English advice
   const fetchAdvice = () => {
@@ -17,6 +19,7 @@ const App = () => {
       .then((res) => {
         const { advice } = res.data.slip;
         setAdvice({ advice: advice, isFetching: false });
+        setQuoteCount((prevCount) => prevCount + 1); // Increment quote count
       })
       .catch((error) => {
         console.log("Error occurred", error);
@@ -31,17 +34,25 @@ const App = () => {
   const rawAdvice = advice.advice.slice(0, -1);
 
   return (
-    <div className="app">
-      {/* English Advice Card */}
-      <div className={`card ${advice.isFetching ? "fetching" : ""}`}>
-        <h1 className="heading">
-          <span>' </span>{rawAdvice}<span> '</span>
-        </h1>
-        <button onClick={fetchAdvice} className="button">
-          <span>Generate Advice</span>
-        </button>
+    <>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="/">Quotes Generator</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div className="app">
+        {/* English Advice Card */}
+        <div className={`card ${advice.isFetching ? "fetching" : ""}`}>
+          <h1 className="heading">
+            <span>' </span>{rawAdvice}<span> '</span>
+          </h1>
+          <button onClick={fetchAdvice} className="button">
+            <span>Generate Advice</span>
+          </button>
+          <p className="quote-count">Quotes Generated: {quoteCount}</p> {/* Display the quote count */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
